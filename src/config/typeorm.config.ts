c60f8d004f3c4f27caa346ea85974fb8a1bcdc87
hou27 <ataj125@gmail.com>
2023-07-29 22:20:37 +0900

    chore: typeorm synchronize 옵션 false로 변경

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,7 +9,7 @@ export const generateTypeOrmConfig: GenerateTypeOrmConfig = (env) => ({
   username: env.DATABASE_USERNAME,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  synchronize: true, // env.NODE_ENV === 'prod',
+  synchronize: false,
   logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 });
