0c7da27502e7ac75f066256b783ea321b1319ef4
hou27 <ataj125@gmail.com>
2023-07-29 22:15:38 +0900

    feat: chapter entity 작성 - #2

---
+++ src/chapters/entities/chapter.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { IsString } from 'class-validator';
+import { Problem } from 'src/problems/entities/problem.entity';
+import { Learning } from 'src/learnings/entities/learning.entity';
+
+@Entity()
+export class Chapter {
+  @PrimaryGeneratedColumn('uuid')
+  id!: number;
+
+  @Column()
+  @IsString()
+  title!: string;
+
+  @Column({ nullable: true })
+  @IsString()
+  help_message?: string;
+
+  @OneToMany((type) => Problem, (problem) => problem.chapter, {
+    nullable: true,
+    onDelete: 'CASCADE',
+  })
+  problems?: Problem[];
+
+  @ManyToOne((type) => Learning, (learning) => learning.chapters, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  learning?: Learning;
+}
