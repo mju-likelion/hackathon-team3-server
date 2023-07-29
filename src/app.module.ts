b56f51815e25b78b37137d2feb85c7f0434a77cd
hou27 <ataj125@gmail.com>
2023-07-29 22:04:11 +0900

    feat: user entity 작성 - #2
    비밀번호는 8자 이상의 영문자여야하며, 하나 이상의 숫자 포함

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { UsersModule } from './users/users.module';
 import { ProblemsModule } from './problems/problems.module';
 import { ChaptersModule } from './chapters/chapters.module';
 import { LearningsModule } from './learnings/learnings.module';
+import { generateTypeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [UsersModule, ProblemsModule, ChaptersModule, LearningsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.dev'
+          : process.env.NODE_ENV === 'prod'
+          ? '.env.prod'
+          : '.env.test',
+      validationSchema,
+    }),
+    TypeOrmModule.forRoot(generateTypeOrmConfig(process.env)),
+    UsersModule,
+    ProblemsModule,
+    ChaptersModule,
+    LearningsModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,59 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { IsEmail, IsString, Matches } from 'class-validator';
+import * as bcrypt from 'bcrypt';
+import { Chapter } from 'src/chapters/entities/chapter.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: number;
+
+  @Column()
+  @IsString()
+  nickname!: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email!: string;
+
+  @Column({ select: false })
+  @IsString()
+  @Matches(/^(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, {
+    message:
+      'Password must be at least 8 characters(en) long, contain 1 number',
+  })
+  password!: string;
+
+  @ManyToMany(() => Chapter)
+  @JoinTable()
+  completed_chapters?: Chapter[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.password) {
+      try {
+        this.password = await bcrypt.hash(this.password, 10);
+      } catch (e) {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkPassword(plainPassword: string): Promise<boolean> {
+    try {
+      return await bcrypt.compare(plainPassword, this.password);
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
