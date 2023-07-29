7c2e9bb694018f80a7ea2db3adb8f9628bb02ec5
hou27 <ataj125@gmail.com>
2023-07-29 20:26:20 +0900

    setting: entity 작성 및 DB 연결을 위한 패키지 설치 - #2
    typeorm mysql2 @nestjs/typeorm class-validator class-transformer bcrypt
    @nestjs/config joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { ProblemsModule } from './problems/problems.module';
+import { ChaptersModule } from './chapters/chapters.module';
+import { LearningsModule } from './learnings/learnings.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProblemsModule, ChaptersModule, LearningsModule],
   controllers: [],
   providers: [],
 })
