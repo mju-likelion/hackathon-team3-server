905aa2fc05e28d0d5b6c65c772ba7c0eb0d7bcfc
hou27 <ataj125@gmail.com>
2023-07-29 22:16:15 +0900

    feat: learning entity 작성 - #2

---
+++ src/learnings/entities/learning.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNumber } from 'class-validator';
+import { Chapter } from 'src/chapters/entities/chapter.entity';
+
+@Entity()
+export class Learning {
+  @PrimaryGeneratedColumn('uuid')
+  id!: number;
+
+  /**
+   * @description
+   * 0: 기초 학습
+   * 1: 심화 학습
+   */
+  @Column()
+  @IsNumber()
+  type!: number;
+
+  @OneToMany((type) => Chapter, (chapter) => chapter.learning, {
+    nullable: true,
+    onDelete: 'CASCADE',
+  })
+  chapters?: Chapter[];
+}
