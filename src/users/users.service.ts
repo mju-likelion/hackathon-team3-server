a4cc97eb14572e25196cfc44a98604a17349cc6e
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-02 06:31:31 +0900

    feat: auth 모듈 구현을 위한 user 모듈 작성 - #4
    create(), findOne() 작성

---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/users.entity';
+import { Repository } from 'typeorm';
+import { CreateDto } from './dto/Create.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(creteDto: CreateDto) {
+    if (await this.findOne(creteDto.email)) {
+      throw new ConflictException('이미 존재하는 사용자 입니다.');
+    }
+    const newUser = this.usersRepository.create(creteDto);
+    await newUser.hashPassword();
+    return await this.usersRepository.save(newUser);
+  }
+
+  async findOne(email: string) {
+    return await this.usersRepository.findOne({
+      where: { email: email },
+    });
+  }
+}
