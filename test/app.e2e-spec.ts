ab26d3e40ce72a72579d86513f326154a6f97519
hou27 <ataj125@gmail.com>
2023-08-03 16:28:16 +0900

    test: jwt passport와 auth user decorator 동작 테스트 - #10

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,32 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await Promise.all([app.close()]);
+  });
+
+  describe('Authentication', () => {
+    let jwtToken: string;
+
+    it('authenticates user with valid credentials and provides a jwt token', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({ email: 'test1@mail.com', password: 'password1' })
+        .expect(201);
+
+      // set jwt token for use in subsequent tests
+      jwtToken = response.body.access_token;
+      expect(jwtToken).toMatch(
+        /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/,
+      ); // jwt regex
+    });
+
+    it('authenticates user with valid jwt token', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/users')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .expect(200);
+      expect(response.body.email).toBe('test1@mail.com');
+    });
   });
 });
