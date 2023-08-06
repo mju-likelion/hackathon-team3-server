2022bb154c807c537b1cebfb1b637f576a489123
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-07 05:40:39 +0900

    feat: openaiService 변경에 따른 problemsService 코드 수정 및 추가 - #19
    - 주관식(빈칸 포함), 객관식 구분해서 채점
    - 주관식(빈칸 포함) 채점 방식은 다음과 같음
    - 제출 답 공백 최적화
    - 제출 답이 정답에 있는 모든 단어를 포함하는 지 체크, 포함하지 않는다면 틀림
    - 정답과 제출 답의 유사성을 비교해 0~100 점으로 반환
    - 점수가 78점 이상이면 정답

--- src/problems/problems.service.ts
+++ src/problems/problems.service.ts
@@ -1,16 +1,17 @@
 import {
   ConflictException,
-  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/entities/users.entity';
-import { Problem } from './entities/problem.entity';
+import { Problem, QuestionType } from './entities/problem.entity';
 import { SubmitDto } from './dtos/submit.dto';
 import { OpenaiService } from '../apis/openai/openai.service';
 import { SubmitResponseDto } from './dtos/submit-response.dto';
+import { ScoreProblemResponseDto } from '../apis/openai/dtos/score-problem-response.dto';
+import { OptimizeStringResponseDto } from '../apis/openai/dtos/optimize-string-response.dto';
 
 @Injectable()
 export class ProblemsService {
@@ -25,28 +26,70 @@ export class ProblemsService {
     problemId: string,
     submitDto: SubmitDto,
   ): Promise<SubmitResponseDto> {
+    const submitResponseDto: SubmitResponseDto = new SubmitResponseDto();
+    submitResponseDto.statusCode = 200;
+    submitResponseDto.message = 'Problem successfully submitted';
+
     try {
       const problem = await this.problemsRepository.findOne({
         where: { id: problemId },
       });
+
       if (!problem) {
-        throw new NotFoundException('invalid problem id');
+        throw new NotFoundException('Invalid problem ID');
       }
-      const result = await this.openaiService.scoreProblem({
-        problem: problem.answer,
-        answer: submitDto.answer,
-        temperature: 0.2,
-      });
-      if (!result) {
-        throw new ConflictException('openai not response');
+
+      if (problem.type === QuestionType.MCQ) {
+        submitResponseDto.isCorrect = problem.answer === submitDto.answer;
+      } else {
+        const optimizeStringResponseDto: OptimizeStringResponseDto =
+          await this.openaiService.optimizeStringSpaces({
+            requestedString: submitDto.answer,
+            temperature: 0.1,
+          });
+        submitDto.answer = optimizeStringResponseDto.optimizedString;
+        const problemAnswerTokens: string[] = problem.answer.split(' ');
+
+        if (
+          !this.isSubmittedAnswerValid(
+            problemAnswerTokens,
+            submitDto.answer.trim(),
+          )
+        ) {
+          submitResponseDto.isCorrect = false;
+        } else {
+          const scoreProblemResponseDto: ScoreProblemResponseDto =
+            await this.openaiService.scoreProblem({
+              originAnswer: problem.answer,
+              submittedAnswer: submitDto.answer,
+              temperature: 0,
+            });
+
+          if (!scoreProblemResponseDto) {
+            throw new ConflictException('OpenAI did not response');
+          }
+
+          submitResponseDto.isCorrect = scoreProblemResponseDto.score >= 78;
+        }
       }
-      const submitResponseDto: SubmitResponseDto = new SubmitResponseDto();
-      submitResponseDto.statusCode = 200;
-      submitResponseDto.message = 'request successfully submitted';
-      submitResponseDto.isCorrect = +result > 8;
+
       return submitResponseDto;
     } catch (e) {
       throw e;
     }
+  }
+
+  isSubmittedAnswerValid(
+    problemAnswerTokens: string[],
+    submittedAnswer: string,
+  ) {
+    let isValuableAnswer = true;
+    for (const answer of problemAnswerTokens) {
+      if (!submittedAnswer.includes(answer)) {
+        isValuableAnswer = false;
+        break;
+      }
+    }
+    return isValuableAnswer;
   }
 }
