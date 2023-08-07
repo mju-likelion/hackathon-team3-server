4ae3d38a6c4adbe3cbeabdddf0493c55824bd26a
hou27 <ataj125@gmail.com>
2023-08-07 18:33:46 +0900

    feat: 챕터 정보 api 구현 - #31
    챕터 내의 문제, 시나리오, 도움말을 반환하는 api

---
+++ src/chapters/chapters.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GetChapterRes } from './dtos/chapter.dto';
+import { User } from 'src/users/entities/users.entity';
+import { Chapter } from './entities/chapter.entity';
+import { parseProblem } from 'src/problems/utils/problem.util';
+
+@Injectable()
+export class ChaptersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    @InjectRepository(Chapter)
+    private chaptersRepository: Repository<Chapter>,
+  ) {}
+
+  async getChapter(user: User, chapterId: string): Promise<GetChapterRes> {
+    try {
+      const userInDb = await this.usersRepository.findOne({
+        where: { id: user.id },
+        relations: {
+          completed_chapters: {
+            learning: true,
+          },
+        },
+      });
+      if (!userInDb) {
+        throw new NotFoundException('User not found');
+      }
+
+      const chapter = await this.chaptersRepository.findOne({
+        where: {
+          id: chapterId,
+        },
+        relations: {
+          problems: true,
+        },
+      });
+      if (!chapter) throw new NotFoundException('Chapter not found');
+      if (chapter.problems.length === 0)
+        throw new NotFoundException('Problems not found within the chapter');
+
+      // TODO: user가 아직 풀지 않은 문제들 중 랜덤으로 3개를 반환
+      // chapter.problems = ???
+
+      // 문제 가공
+      chapter['parsed_problem'] = chapter.problems.map((problem) => {
+        const { id, type, scenario, content, answer } = problem;
+        const parsedContent = parseProblem(content, type);
+        return {
+          id,
+          type,
+          scenario,
+          question: parsedContent,
+          answer,
+        };
+      });
+
+      // chapter에서 가공 전 problem 제거
+      delete chapter.problems;
+
+      return {
+        statusCode: 200,
+        message: 'Chapter successfully retrieved',
+        chapter,
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
+}
