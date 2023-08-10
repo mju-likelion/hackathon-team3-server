4916e4c83cd1f2069d175cda04f3ded1ea4c0363
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-10 19:17:09 +0900

    feat: openai에 Rate Limiting 구현 - #49
    1분에 10번으로 요청 제한

--- src/apis/openai/openai.module.ts
+++ src/apis/openai/openai.module.ts
@@ -2,14 +2,26 @@ import { Module } from '@nestjs/common';
 import { OpenaiService } from './openai.service';
 import { ConfigModule } from '@nestjs/config';
 import openaiConfig from './config/openaiConfig';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [openaiConfig],
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
   ],
-  providers: [OpenaiService],
+  providers: [
+    OpenaiService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [OpenaiService],
 })
 export class OpenaiModule {}
