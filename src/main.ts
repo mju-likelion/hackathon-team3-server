7e3c770ce17d8b7035fc9eff6dc13120453bef52
hou27 <ataj125@gmail.com>
2023-08-13 02:14:40 +0900

    !HOTFIX: set-cookie 동작을 위한 추가 설정 - #69
    main에 app.enableCors({origin: true, credentials: true});

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
