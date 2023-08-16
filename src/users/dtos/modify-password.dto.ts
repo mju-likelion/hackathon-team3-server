76c8729753fb7a6f1357f074fb96c5908c203aaa
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-16 20:34:04 +0900

    feat: dto에 validation 추가 - #89

--- src/users/dtos/modify-password.dto.ts
+++ src/users/dtos/modify-password.dto.ts
@@ -1,10 +1,14 @@
 import { PickType } from '@nestjs/mapped-types';
 import { User } from '../entities/users.entity';
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 import { CoreRes } from 'src/common/dtos/Response.dto';
 
 export class ModifyPasswordReq extends PickType(User, ['password']) {
   @IsString()
+  @Matches(/^(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, {
+    message:
+      'Password must be at least 8 characters(en) long, contain 1 number',
+  })
   oldPassword!: string;
 }
 
