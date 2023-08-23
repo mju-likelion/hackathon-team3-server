038c8eced0bdc3a700cb69b362072b4aa0f29958
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-24 05:27:09 +0900

    feat: trust proxy를 위해 app이 express를 사용하게 설정 - #102

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
