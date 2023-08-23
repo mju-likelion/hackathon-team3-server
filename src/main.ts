abe64c0dd34d21403b433ddd61c37ebb0fd83d0b
Kim Daehyeon <daehyeon3351@gmail.com>
2023-08-24 05:28:06 +0900

    feat: trust proxy 설정 - #102

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', true);
   app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
