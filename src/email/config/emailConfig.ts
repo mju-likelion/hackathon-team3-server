08ae352341bf448ade773e357e3a554a0a10355e
Kim Daehyeon <daehyeon3351@gmail.com>
2023-09-11 22:39:02 +0900

    feat: verificationUrl을 환경 변수로 등록 - #112

--- src/email/config/emailConfig.ts
+++ src/email/config/emailConfig.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('email', () => ({
   baseEmail: process.env.SENDGRID_BASE_EMAIL,
   apiKey: process.env.SENDGRID_API_KEY,
+  emailVerificationUrl: process.env.EMAIL_VERIFICATION_URL,
 }));
