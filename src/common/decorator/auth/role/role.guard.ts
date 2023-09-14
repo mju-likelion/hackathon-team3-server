727eb736d31ab06449dcde58b43f1b4c67cb1638
Kim Daehyeon <daehyeon3351@gmail.com>
2023-09-14 20:09:56 +0900

    feat: role 체크를 위한 RoleGuard, Role 데코레이터 구현 - #116

---
+++ src/common/decorator/auth/role/role.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { UserRole } from '../../../../users/entities/users.entity';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<UserRole>('roles', context.getHandler());
+
+    if (!roles) {
+      throw new NotFoundException('Roles not defined');
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (roles.includes('ANY')) {
+      return true;
+    }
+    if (user.role !== roles[0]) {
+      throw new UnauthorizedException('Unauthorized User');
+    }
+    return roles.includes(user.role);
+  }
+}
