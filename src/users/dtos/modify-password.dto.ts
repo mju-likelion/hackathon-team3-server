cf97ec2653ba0a61ce598eccf4fd7f8ad9d52fce
Kim Daehyeon <daehyeon3351@gmail.com>
2023-10-18 03:40:13 +0900

    refactor: ResponseDto 리팩터링에 따른 Res 제거 - #123

--- src/users/dtos/modify-password.dto.ts
+++ src/users/dtos/modify-password.dto.ts
@@ -1,11 +1,8 @@
 import { PickType } from '@nestjs/mapped-types';
 import { User } from '../entities/users.entity';
 import { IsString } from 'class-validator';
-import { CoreRes } from 'src/common/dtos/Response.dto';
 
 export class ModifyPasswordReq extends PickType(User, ['password']) {
   @IsString()
   oldPassword!: string;
 }
-
-export class ModifyPasswordRes extends CoreRes {}
